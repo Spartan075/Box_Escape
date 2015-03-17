bdf2781cd7b0016f9f0615fdfdeb9fc9e78a38f1
Spartan075 <andrewcoole1996@gmail.com>
2015-03-16 19:28:58 -0700

    Initial commit with all files Added rest of files after oopsie Fixed Ignore. On the right track

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowObject : MonoBehaviour {
+
+	public Transform objectToFollow;
+	public Vector3 offset;
+	
+
+
+	void Update () {
+	
+		transform.position = objectToFollow.position + offset;
+
+	}
+}
---
+++ Assets/Scripts/MovePlayerBox.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayerBox : MonoBehaviour {
+
+	public float secondsToRoll;
+	public float rollStep;
+	public float waitTime;
+	public bool canRotate;
+
+	public Vector3 frontPivot;
+	public Vector3 backPivot;
+	public Vector3 leftPivot;
+	public Vector3 rightPivot;
+	
+	public Rigidbody rb;
+	
+	void Update() {
+
+
+		float h = Input.GetAxisRaw("Horizontal");
+		float v = Input.GetAxisRaw("Vertical");
+
+		if(h == 1 && canRotate)
+			StartCoroutine(rollAroundZForSeconds(rightPivot, -1));
+
+		if(h == -1 && canRotate)
+			StartCoroutine(rollAroundZForSeconds(leftPivot, 1));
+
+		if(v == 1 && canRotate)
+			StartCoroutine(rollAroundXForSeconds(frontPivot, 1));
+
+		if(v == -1 && canRotate)
+			StartCoroutine(rollAroundXForSeconds(backPivot, -1));
+
+	}
+	
+	
+
+
+
+
+
+
+	IEnumerator rollAroundXForSeconds(Vector3 pivot, int rollDir) {
+
+		canRotate = false;
+
+		pivot = transform.position + pivot;
+		
+		
+		for(int x = 0; x <= (secondsToRoll * 60) - 3; x++) {
+			
+			transform.RotateAround(pivot, Vector3.right, rollStep * rollDir);
+			
+			yield return null;
+			
+		}
+
+		yield return new WaitForSeconds(waitTime);
+		canRotate = true;
+		
+	}
+
+	IEnumerator rollAroundZForSeconds(Vector3 pivot, int rollDir) {
+		
+		canRotate = false;
+		
+		pivot = transform.position + pivot;
+		
+		
+		for(int x = 0; x <= (secondsToRoll * 60) - 3; x++) {
+			
+			transform.RotateAround(pivot, Vector3.forward, rollStep * rollDir);
+			
+			yield return null;
+			
+		}
+
+		yield return new WaitForSeconds(waitTime);
+		canRotate = true;
+		
+	}
+
+}
