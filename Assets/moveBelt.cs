83fbbe0196c3ff52902f6ab374f4b3362e466d95
Spartan075 <andrewcoole1996@gmail.com>
2015-03-16 22:33:34 -0700

    Added a test conveyor belt as well as a potential consant-torque movement system.

---
+++ Assets/moveBelt.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveBelt : MonoBehaviour {
+
+	public float moveSpeed;
+
+
+	Rigidbody rb;
+
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update() {
+
+		Vector3 vel = rb.velocity;
+
+		vel.x = moveSpeed;
+
+		rb.velocity = vel;
+
+		if(transform.position.x > 30) {
+			Vector3 pos = transform.position;
+
+			pos.x = -30;
+
+			transform.position = pos;
+		}
+	}
+}
---
+++ Assets/testBoxMove.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testBoxMove : MonoBehaviour {
+
+	public float rollForce;
+
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float v = Input.GetAxisRaw("Vertical");
+		float h = Input.GetAxisRaw("Horizontal");
+
+		if(h == 0)
+			rb.AddRelativeTorque(new Vector3(v * rollForce * Time.deltaTime, 0, 0));
+
+		if(v == 0)
+			rb.AddRelativeTorque(new Vector3(0, 0, h * rollForce * Time.deltaTime));
+	
+	}
+}
