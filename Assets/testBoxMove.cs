6448d7ac094fa42ebf2bffe5479421fc1171c536
Spartan075 <andrewcoole1996@gmail.com>
2015-03-17 14:31:28 -0700

    Reverting to regular torque, plus addition of parent to cube.

--- Assets/testBoxMove.cs
+++ Assets/testBoxMove.cs
@@ -5,15 +5,9 @@ public class testBoxMove : MonoBehaviour {
 
 	public float rollForce;
 
-	Rigidbody rb;
-
-	// Use this for initialization
-	void Start () {
-
-		rb = GetComponent<Rigidbody>();
+	public Rigidbody rb;
 	
-	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
@@ -21,10 +15,10 @@ public class testBoxMove : MonoBehaviour {
 		float h = Input.GetAxisRaw("Horizontal");
 
 		if(h == 0)
-			rb.AddRelativeTorque(new Vector3(v * rollForce * Time.deltaTime, 0, 0));
+			rb.AddTorque(transform.right * v * rollForce * Time.deltaTime);
 
 		if(v == 0)
-			rb.AddRelativeTorque(new Vector3(0, 0, h * rollForce * Time.deltaTime));
+			rb.AddTorque(transform.forward * -h * rollForce * Time.deltaTime);
 	
 	}
 }
